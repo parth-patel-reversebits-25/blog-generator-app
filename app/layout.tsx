522291b788e65a2ecb3519223b82ce9c722a6c25
Parth Patel <parth.patel.reversebits@gmail.com>
2025-09-20 16:22:24 +0530

    ✨feature: added favicon icons and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,31 @@
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import { Analytics } from '@vercel/analytics/next'
-import './globals.css'
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import { Analytics } from "@vercel/analytics/next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
-}
+  title: "Blog Generator App",
+  description: "Created by reverseBits.tech",
+  icons: {
+    icon: "/favicon-icons/favicon.ico",
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
+    <html lang="en" suppressHydrationWarning>
+      <body
+        suppressHydrationWarning
+        className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}
+      >
         {children}
         <Analytics />
       </body>
     </html>
-  )
+  );
 }
